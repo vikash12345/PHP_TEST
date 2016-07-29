14461b5ca1781ad7281563e116c3946e45c1922b
vikash12345 <vikash@backgroundcheck.email>
2016-07-29 16:51:57 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,5 +1,5 @@
-1. <?php
-2. $url = 'http://www.oooff.com';
-3. $output = file_get_contents($url); 
-4. echo $output;
-5. ?>
+<?php
+$url = 'http://www.oooff.com';
+$output = file_get_contents($url); 
+echo $output;
+?>
